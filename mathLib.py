693f1c11ddc5813d9b314c811a66d204f3794445
Rudranil Maity <48630058+Pegasus-01@users.noreply.github.com>
2020-06-23 20:34:48 +0530

    Add files via upload

---
+++ mathLib.py
@@ -0,0 +1,14 @@
+import math
+#using math library and some basic function of this
+print(abs(-45))
+print(math.fabs(-45))
+
+print(math.ceil(-45.28))
+print(math.ceil(200.12))   #ceil prints the nearest maximum integer
+
+
+print(math.floor(-45.12))
+print(math.floor(200.23)) #floor prints the nearest minimum integer
+
+print(math.acos(1))
+print(max(10, 20, 30, 40, 50))
